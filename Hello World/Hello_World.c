8f024bde038dbe2eaff317b4a63f992fc3ce7b6f
Josh <Stallworth1995@yahoo.com>
2021-05-10 15:37:54 -0700

    Dev of Hello World in C: Finished. This program was just a basic hello world program made in C.

---
+++ Hello World/Hello_World.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+//Simple hello world in the main function
+int main()
+{
+    printf("Hello World!\n"); //Calls the printf function to display text
+
+    return 0; //Indicates succesful program execution
+    
+} //Ends the function of main
